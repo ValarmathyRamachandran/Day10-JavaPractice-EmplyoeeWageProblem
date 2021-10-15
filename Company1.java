2de7750618651bf6d4e34780f2917cef1b9c5b8d
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-10-15 16:17:42 +0530

    Added interface to calculate Emp wages

--- Company1.java
+++ Company1.java
@@ -1,6 +1,6 @@
 package EmpWage;
 
-public class Company1 extends EmpWageBuilderUC10 {
+public class Company1 extends EmpWageBuilderUC10 implements Company2 {
 	
 	int EMP_RATE_PER_HOUR = 20;
 	int NUM_OF_WORKING_DAYS = 20;
